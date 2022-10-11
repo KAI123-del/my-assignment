a297d6c584e6874e5b185f7cc940e6a05066d5f5
KAI123-del <you@example.com>
2022-10-12 03:51:44 +0530

    added back button on user page

--- src/Components/UserScreen.jsx
+++ src/Components/UserScreen.jsx
@@ -1,16 +1,41 @@
+import { Button, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useGlobalState } from "../store";
+import { useNavigate } from "react-router-dom";
 
 function UserScreen() {
-  const { randomData, fetchProduct } = useGlobalState();
+  const { jwtToken, randomData, fetchProduct } = useGlobalState();
+  let navigate = useNavigate();
 
   useEffect(() => {
     fetchProduct();
   }, []);
 
-  console.log("data", randomData);
-
-  return <img src={randomData[0]?.image} />;
+  return (
+    <>
+      {jwtToken ? (
+        <img src={randomData[0]?.image} />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "80vh",
+          }}>
+          {" "}
+          <Typography variant="h4">NOT AN ADMIN USER</Typography>
+          <Button
+            onClick={() => navigate("/")}
+            variant="contained"
+            sx={{ width: "10vw", mt: 3 }}>
+            Go Back
+          </Button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default UserScreen;
